53e86a53f2024a91d559eeeaf0129969c6ce7869
Omar Olivares <ofjolivares@gmail.com>
2019-06-10 16:06:39 -0400

    Añadir Espiral Cónica de Papus

--- interface/tarea02.py
+++ interface/tarea02.py
@@ -20,8 +20,6 @@ from matplotlib import animation
 
 
 # Hélice Conica
-def conico_papus():
-  pass
 
 
 # Hélice Conica
@@ -182,7 +180,42 @@ def Hipopoda():
     ax.legend()
     plt.show()
     pass
+
 # Espiral Cónica de Papus
+def conico_papus():
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    def gen(n):
+        t = -6 * np.pi
+
+        a1 = 30
+
+        a = 15
+        for t in np.linspace(-5 * np.pi, 5 * np.pi, 200):
+            yield np.array([a1 * np.sin(a) * t * np.cos(t), a1 * np.sin(a) * t * np.sin(t), a1 * np.cos(a) * t])
+
+    def update(num, data, line):
+        line.set_data(data[:2, :num])
+        line.set_3d_properties(data[2, :num])
+
+    N = 200
+    data = np.array(list(gen(N))).T
+    line, = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])
+
+    # Setting the axes properties
+    ax.set_xlim3d([-400.0, 400.0])
+    ax.set_xlabel('X')
+
+    ax.set_ylim3d([-400.0, 400.0])
+    ax.set_ylabel('Y')
+
+    ax.set_zlim3d([-400.0, 400.0])
+    ax.set_zlabel('Z')
+
+    ani = animation.FuncAnimation(fig, update, N, fargs=(data, line), interval=1000 / N, blit=False, repeat=False)
+
+    plt.show()
 
 # Curva de Arquitas
 
@@ -206,8 +239,8 @@ if __name__ == '__main__':
     hipopoda_button = tk.Button(master=frame, text="Hipopoda", command=Hipopoda, image=hipopoda_im)
     hipopoda_button.pack(side=tk.BOTTOM, padx=10, pady=10)
 
-    imagen_viviani =  PhotoImage(file="Viviani_curve.png")
-    viviani = Button(master=frame,image=imagen_viviani,width=100,height=100,command=curva_de_viviani)
+    imagen_viviani =  tk.PhotoImage(file="Viviani_curve.png")
+    viviani = tk.Button(master=frame,image=imagen_viviani,width=100,height=100,command=curva_de_viviani)
     viviani.pack()
     Conica_im = tk.PhotoImage(file="helice_conica.gif")
     conica_button = tk.Button(master=frame, text="Hélice Cónica", command=helice_conica, image=Conica_im)
@@ -219,7 +252,7 @@ if __name__ == '__main__':
 
     curva_de_ejemplo = tk.Button(master=frame, text="conica de papus", command=conico_papus)
     curva_de_ejemplo.pack(side=tk.BOTTOM, padx=10, pady=10)
-    Conica_im = tk.PhotoImage(file="conica_papu.gif")
+    # Conica_im = tk.PhotoImage(file="conica_papu.gif")
     conica_button = tk.Button(master=frame, text="conica de papus", command=conico_papus, image=Conica_im)
     conica_button.pack(side=tk.BOTTOM, padx=10, pady=10)
 
